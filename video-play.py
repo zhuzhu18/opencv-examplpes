c0cea020cac08fffa0e9f870fb1f3ff92a62aae5
zhuzhu <zhuzhu18@aliyun.com>
2020-01-06 20:42:19 +0800

    播放视频

--- video-play.py
+++ video-play.py
@@ -1,8 +1,9 @@
-impport cv2
+import cv2
 
 cap = cv2.VideoCapture('/home/zhuzhu/视频/vlc-record-2019-05-22-19h01m58s-AlphaPose_together.avi-.mp4')
 
 while True:
   ret, frame = cap.read()
-  if cv2.waitKey(30) & ord('q') == 0xFF:
+  cv2.imshow('frame', frame)
+  if cv2.waitKey(30) & 0xFF == ord('q'):
     break
